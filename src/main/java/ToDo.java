9717a96072e00db5c82ce8a4059f846b16e425a4
MobinNoroozi <mobinsmac@mobins-mbp.mynetworksettings.com>
2023-12-07 21:29:11 -0500

    Setters and getters

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,6 +1,7 @@
 package main.java;
 
 public class ToDo {
+    //attributes
     private String name;
     private String time;
     private String date;
@@ -8,6 +9,7 @@ public class ToDo {
     private String repetition;
     private String deadline;
 
+    //costructor
     public ToDo() {
         this.name = "";
         this.time = "";
@@ -15,5 +17,53 @@ public class ToDo {
         this.duration = 0;
         this.repetition = "";
         this.deadline = "";
+    }
+    // costructor override
+    public ToDo(String name, String time, String date, String repetition, String deadline, double duration){
+        this.name = name;
+        this.time = time;
+        this.date = date;
+        this.deadline = deadline;
+        this.duration = duration;
+        this.repetition = repetition;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setTime(String time){
+        this.time = time;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+    public void setDuration(double duration){
+        this.duration = duration;
+    }
+    public void setRepetition(String repetition){
+        this.repetition = repetition;
+    }
+    public void setDeadline(String deadline){
+        this.deadline = deadline;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+    public String getTime(){
+        return time;
+    }
+    public String getDate(){
+        return date;
+    }
+    public double getDuration(){
+        return duration;
+    }
+    public String getRepetition(){
+        return repetition;
+    }
+    public String getDeadline(){
+        return deadline;
     }
 }
