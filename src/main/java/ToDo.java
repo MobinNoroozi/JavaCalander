fae00799d38db388fbd3715189371a689f3e0775
Lincoln Howard Jr <lincoln.c.howard.jr@gmail.com>
2023-12-07 21:12:44 -0500

    Scaffold a Todo type

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,5 +1,19 @@
 package main.java;
 
 public class ToDo {
-    
+    private String name;
+    private String time;
+    private String date;
+    private double duration;
+    private String repetition;
+    private String deadline;
+
+    public ToDo() {
+        this.name = "";
+        this.time = "";
+        this.date = "";
+        this.duration = 0;
+        this.repetition = "";
+        this.deadline = "";
+    }
 }
